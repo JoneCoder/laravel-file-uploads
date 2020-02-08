6f2347ce11decaee4ed21d0b44845dd24f07157b
JoneCoder <mohummadshorifulislam@gmail.com>
2020-02-09 01:39:06 +0600

    add file_uploads table schema, make model and controller

---
+++ database/migrations/2020_02_08_193124_create_file_uploads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileUploadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_uploads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->default('default.jpg')->comment('file must be a image or pdf');
+            $table->string('type')->default('jpg')->comment('file type must be a image or pdf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_uploads');
+    }
+}
